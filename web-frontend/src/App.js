5ac680643a6dca0d793934f0ba13643d84cd877b
Benoît Hubert <benoithubert@gmail.com>
2022-04-01 01:55:17 +0200

    Show authentication state on client app load

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -1,7 +1,7 @@
-import { gql, useQuery } from '@apollo/client';
+import { gql, useQuery } from "@apollo/client";
 
-import './App.css';
-import Login from './pages/Login';
+import "./App.css";
+import Login from "./pages/Login";
 
 const GET_CURRENT_USER = gql`
   query GetCurrentUser {
@@ -17,9 +17,13 @@ function App() {
   const { loading, error, data } = useQuery(GET_CURRENT_USER);
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
   return (
     <div className="App">
+      {data?.currentUser ? (
+        <p>Authenticated as {data.currentUser.email}</p>
+      ) : (
+        <p>not authenticated</p>
+      )}
       <Login />
     </div>
   );
