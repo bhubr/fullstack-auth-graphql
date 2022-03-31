531e5d602f0a699d5b82f0df413e18ed307c6d95
Benoît Hubert <benoithubert@gmail.com>
2022-03-31 08:24:23 +0200

    Remove dummy data insertion

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,26 +1,11 @@
 const mongoose = require('mongoose');
 
-const MovieModel = require('./models/Movie');
-const UserModel = require('./models/User');
+// const MovieModel = require('./models/Movie');
+// const UserModel = require('./models/User');
 
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/auth_dev');
   console.log('connected');
-
-  const movie = new MovieModel({
-    title: 'Avatar',
-    picture: 'https://img.com/av.jpg',
-    releaseYear: 2009
-  })
-  await movie.save();
-
-
-  const user = new UserModel({
-    email: 'test01@email.com',
-    password: 'abc',
-    role: 'user'
-  })
-  await user.save();
 
   
 }
