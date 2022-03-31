6c98af9411223caf744a51e3ed441c9e990f842c
Benoît Hubert <benoithubert@gmail.com>
2022-04-01 01:40:28 +0200

    Send query from client app to get connected user data

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -1,7 +1,23 @@
+import { gql, useQuery } from '@apollo/client';
+
 import './App.css';
 import Login from './pages/Login';
 
+const GET_CURRENT_USER = gql`
+  query GetCurrentUser {
+    currentUser {
+      _id
+      email
+      role
+    }
+  }
+`;
+
 function App() {
+  const { loading, error, data } = useQuery(GET_CURRENT_USER);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
   return (
     <div className="App">
       <Login />
